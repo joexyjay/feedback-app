2390220f1ac544c1457839948b408f0f4f824e9a
joexy <josephethethowo@gmail.com>
2023-12-02 15:31:58 +0100

    add delete feedback functionality

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,22 @@
+import {FaTimes} from "react-icons/fa"
 import propTypes from 'prop-types'
 import Card from "./shared/Card"
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
+
 
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
+        <button className="close" onClick={()=>handleDelete(item.id)}>
+            <FaTimes />
+        </button>
         <div className="text-display">{item.text}</div>
     </Card>
   )
 }
 
 FeedbackItem.propTypes = {
-    item :propTypes.object.isRequired
+    item :propTypes.object.isRequired,
+    handleDelete: propTypes.func.isRequired
 }
 export default FeedbackItem
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,18 @@
 import propTypes from "prop-types"
 import FeedbackItem from "./FeedbackItem"
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
     if(!feedback || feedback.length == 0) return (<div>No feedback to display</div>)
   return (
     <div className="feedback-list">
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item} />
+            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
         ))}
     </div>
   )
 }
 FeedbackList.propTypes = {
   feedback: propTypes.array.isRequired,
+    handleDelete: propTypes.func.isRequired,
 }
 
 export default FeedbackList

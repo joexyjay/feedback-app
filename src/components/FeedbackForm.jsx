5b8ec782060a5687f41fd293275081ea6c75ccbb
joexy <josephethethowo@gmail.com>
2023-12-05 11:04:26 +0100

    add disabled button state

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,8 +3,22 @@ import Card from "./shared/Card"
 import Button from "./shared/Button"
 function FeedbackForm() {
     const [text, setText] = useState("")
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState("")
 
     const handleTextChange = (e) => {
+        if (text === "") {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else 
+            if(text !== "" && text.trim().length < 10) {
+                setBtnDisabled(true)
+                setMessage("Please enter at least 10 characters")
+            }
+        else {
+            setBtnDisabled(false)
+            setMessage(null)
+        }
         setText(e.target.value)
     }
 
@@ -18,8 +32,9 @@ function FeedbackForm() {
                 type="text" 
                 placeholder="write a review" 
                 value={text}/>
-                <Button type="submit">Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+            {message && <div className="message">{message}</div>}
         </form>
     </Card>
   )

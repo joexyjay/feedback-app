ecc0b6dd9df5fb74cf45c1a8acd7c6a24ef58e7d
joexy <josephethethowo@gmail.com>
2023-12-06 13:46:49 +0100

    about page add

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/shared/Card"
+function AboutPage() {
+  return (
+    <Card>
+        <div className="about">
+            <h1>About This Project</h1>
+            <p>This is a React App to leave Feedback for a product or service</p>
+            <p>Version: 1.0.0</p>
+        </div>
+    </Card>
+  )
+}
+export default AboutPage

0ba54df6565597180bc4d0ba096690ea0daf4873
joexy <josephethethowo@gmail.com>
2023-12-12 18:19:38 +0100

    add json mock backend and spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,12 +1,14 @@
 import {motion, AnimatePresence} from "framer-motion"
 import { useContext } from "react"
 import FeedbackItem from "./FeedbackItem"
+import Spinner from "./shared/Spinner"
 import FeedbackContext from "../context/FeedbackContext"
 function FeedbackList() {
-   const { feedback } = useContext(FeedbackContext)
+   const { feedback, isLoading} = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length == 0) return (<div>No feedback to display</div>)
-  return (
+    if(!isLoading && (!feedback || feedback.length == 0)) return (<div>No feedback to display</div>)
+
+  return isLoading ? (<Spinner />) : (
     <div className="feedback-list">
         <AnimatePresence>
             {feedback.map((item) => (
---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from "../../assets/spinner.gif"
+function Spinner() {
+  return (
+   <img 
+   src={spinner} 
+   alt="Loading.." 
+    style={{ width: "200px", margin: "auto", display: "block" }}
+   />
+  )
+}
+export default Spinner

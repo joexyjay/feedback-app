64452291eee7f7c84b7f932ec649c533eff787aa
joexy <josephethethowo@gmail.com>
2023-12-13 11:53:54 +0100

    add feedback to JsonDB

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid"
 import {createContext, useState, useEffect} from 'react';
 import propTypes from 'prop-types';
 
@@ -24,9 +23,17 @@ export const FeedbackProvider = ({children}) => {
     }
 
     //Add feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch("http://localhost:5000/feedback", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(newFeedback),
+        
+        })
+        const data = await response.json()
+        setFeedback([data, ...feedback])
       }
 
       //Delete feedback
